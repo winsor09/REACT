bd00263bb80c2d0d0f8efca1e2bc197bce040d8f
winsor09 <202092@student.asd20.org>
2018-12-07 13:36:27 -0700

    gamemanager

--- REACT/REACT/Assets/Scripts/GameManager.cs
+++ REACT/REACT/Assets/Scripts/GameManager.cs
@@ -2,11 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
+    float timer = 0f;
+    float time2goal = 6000f;
     static GameManager instance;
 
     public static GameManager Instance
-    { get { return instance ?? ((instance = new GameManager())); } }
+    {
+        get { return instance ?? ((instance = new GameManager())); }
+    }
 
+    public Character MyCharacter
+    {
+        get; set;
+    }
+
+    private GameManager()
+    {
+        Object.DontDestroyOnLoad(new GameObject("Updater", typeof(Updater)));
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+    }
+
+    class Updater : MonoBehaviour
+    {
+        private void Update()
+        {
+            instance.Update();
+        }
+    }
 }
