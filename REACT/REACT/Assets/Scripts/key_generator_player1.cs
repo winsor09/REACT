bb0e869532b47ba38be3090d555229625fda31ba
Franejo <franejotwitch@gmail.com>
2018-12-07 12:38:35 -0700

    Added more code

--- REACT/REACT/Assets/Scripts/key_generator_player1.cs
+++ REACT/REACT/Assets/Scripts/key_generator_player1.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class key_generator_player1 : MonoBehaviour {
+    int rand;
+    string currentval;
+    List<string> Key = new List<string> { "q", "w", "e", "r", "a", "s", "d", "f", "z", "x", "c" };
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        rand = Random.Range(1, 12);
+        currentval = Key[rand];
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+        if (currentval == "q")
+        {
+
+        }
+        if (currentval == "w")
+        {
+
+        }
+        if (currentval == "e")
+        {
+
+        }
+        if (currentval == "r")
+        {
+
+        }
+        if (currentval == "a")
+        {
+
+        }
+        if (currentval == "s")
+        {
+
+        }
+        if (currentval == "d")
+        {
+
+        }
+        if (currentval == "f")
+        {
+
+        }
+        if (currentval == "z")
+        {
+
+        }
+        if (currentval == "x")
+        {
+
+        }
+        if (currentval == "c")
+        {
+
+        }
+
+
+    }
 }
+
--- REACT/REACT/Assets/Scripts/key_generator_player2.cs
+++ REACT/REACT/Assets/Scripts/key_generator_player2.cs
@@ -3,14 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class key_generator_player2 : MonoBehaviour {
+    int rand;
+    string currentval;
+    List<string> Key = new List<string> { "u", "i", "o", "p", "h", "j", "k", "l", "b", "n", "m" };
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        rand = Random.Range(1, 12);
+        currentval = Key[rand];
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (currentval == "u")
+        {
+
+        }
+        if (currentval == "i")
+        {
+
+        }
+        if (currentval == "o")
+        {
+
+        }
+        if (currentval == "p")
+        {
+
+        }
+        if (currentval == "h")
+        {
+
+        }
+        if (currentval == "j")
+        {
+
+        }
+        if (currentval == "k")
+        {
+
+        }
+        if (currentval == "l")
+        {
+
+        }
+        if (currentval == "b")
+        {
+
+        }
+        if (currentval == "n")
+        {
+
+        }
+        if (currentval == "m")
+        {
+
+        }
+    }
 }
