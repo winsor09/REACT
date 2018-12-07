f2f16ad175ea2ca1b61ac904f6abc450aa6b0db8
Franejo <franejotwitch@gmail.com>
2018-12-07 13:18:47 -0700

    Legit so much code

--- REACT/REACT/Assets/Scripts/key_generator_player1.cs
+++ REACT/REACT/Assets/Scripts/key_generator_player1.cs
@@ -5,7 +5,18 @@ public class key_generator_player1 : MonoBehaviour {
     int rand;
     string currentval;
     List<string> Key = new List<string> { "q", "w", "e", "r", "a", "s", "d", "f", "z", "x", "c" };
-
+    [SerializeField]
+    GameObject q;
+    GameObject w;
+    GameObject e;
+    GameObject r;
+    GameObject a;
+    GameObject s;
+    GameObject d;
+    GameObject f;
+    GameObject z;
+    GameObject x;
+    GameObject c;
     // Use this for initialization
     void Start ()
     {
@@ -18,49 +29,76 @@ public class key_generator_player1 : MonoBehaviour {
     {
         if (currentval == "q")
         {
-
+            ResetCharacters();
+            q.SetActive(true);
         }
         if (currentval == "w")
         {
-
+            ResetCharacters();
+            w.SetActive(true);
         }
         if (currentval == "e")
         {
-
+            ResetCharacters();
+            e.SetActive(true);
         }
         if (currentval == "r")
         {
-
+            ResetCharacters();
+            r.SetActive(true);
         }
         if (currentval == "a")
         {
-
+            ResetCharacters();
+            a.SetActive(true);
         }
         if (currentval == "s")
         {
-
+            ResetCharacters();
+            s.SetActive(true);
         }
         if (currentval == "d")
         {
-
+            ResetCharacters();
+            d.SetActive(true);
         }
         if (currentval == "f")
         {
-
+            ResetCharacters();
+            f.SetActive(true);
         }
         if (currentval == "z")
         {
-
+            ResetCharacters();
+            z.SetActive(true);
         }
         if (currentval == "x")
         {
-
+            ResetCharacters();
+            x.SetActive(true);
         }
         if (currentval == "c")
         {
-
+            ResetCharacters();
+            c.SetActive(true);
         }
 
+
+    }
+
+    void ResetCharacters()
+    {
+        q.SetActive(false);
+        w.SetActive(false);
+        e.SetActive(false);
+        r.SetActive(false);
+        a.SetActive(false);
+        s.SetActive(false);
+        d.SetActive(false);
+        f.SetActive(false);
+        z.SetActive(false);
+        x.SetActive(false);
+        c.SetActive(false);
 
     }
 }
--- REACT/REACT/Assets/Scripts/key_generator_player2.cs
+++ REACT/REACT/Assets/Scripts/key_generator_player2.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class key_generator_player2 : MonoBehaviour {
     int rand;
     string currentval;
     List<string> Key = new List<string> { "u", "i", "o", "p", "h", "j", "k", "l", "b", "n", "m" };
+
+    [SerializeField]
+    GameObject u;
+    GameObject i;
+    GameObject o;
+    GameObject p;
+    GameObject h;
+    GameObject j;
+    GameObject k;
+    GameObject l;
+    GameObject b;
+    GameObject n;
+    GameObject m;
 
     // Use this for initialization
     void Start () {
@@ -19,47 +34,83 @@ public class key_generator_player2 : MonoBehaviour {
     {
         if (currentval == "u")
         {
-
+            ResetCharacters();
+            u.SetActive(true);
         }
         if (currentval == "i")
         {
+            ResetCharacters();
+            i.SetActive(true);
 
         }
         if (currentval == "o")
         {
-
+            ResetCharacters();
+            o.SetActive(true);
         }
         if (currentval == "p")
         {
+            ResetCharacters();
+            p.SetActive(true);
 
         }
         if (currentval == "h")
         {
+            ResetCharacters();
+            h.SetActive(true);
 
         }
         if (currentval == "j")
         {
+            ResetCharacters();
+            j.SetActive(true);
 
         }
         if (currentval == "k")
         {
+            ResetCharacters();
+            k.SetActive(true);
 
         }
         if (currentval == "l")
         {
+            ResetCharacters();
+            l.SetActive(true);
 
         }
         if (currentval == "b")
         {
+            ResetCharacters();
+            b.SetActive(true);
 
         }
         if (currentval == "n")
         {
+            ResetCharacters();
+            n.SetActive(true);
 
         }
         if (currentval == "m")
         {
+            ResetCharacters();
+            m.SetActive(true);
 
         }
+    }
+
+    void ResetCharacters()
+    {
+        u.SetActive(false);
+        i.SetActive(false);
+        o.SetActive(false);
+        p.SetActive(false);
+        h.SetActive(false);
+        j.SetActive(false);
+        k.SetActive(false);
+        l.SetActive(false);
+        b.SetActive(false);
+        n.SetActive(false);
+        m.SetActive(false);
+
     }
 }
